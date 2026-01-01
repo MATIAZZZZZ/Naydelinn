0f172c619e5f060d5213a5d63c0164490879a903
MATIAZZZZZ <frankdiegomatias4567@gmail.com>
2025-12-31 22:05:41 -0500

    Delay removal of .container class on page load
    Add a delay before removing the .container class from the body to allow animations to start.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-onload = () => {
+window.onload = () => {
+  // Remueve la clase .container del body cuando la página ha cargado
+  // para iniciar las animaciones si están pausadas por defecto
+  const c = setTimeout(() => {
     document.body.classList.remove("container");
-  };
+    clearTimeout(c);
+  }, 1000);
+};
